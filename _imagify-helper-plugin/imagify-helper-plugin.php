51528b16114f88c98ec47f3b5f20e667e926a748
Git I Hate You <git@screenfeed.fr>
2020-01-14 00:53:35 +0100

    Improved plugins template - File header with new entries. - Modified the namespace. - Replaced the `or` statement by `||`. - Put the `add_action()` before the function. - Added a `@since` tag to the function DocBlock.

--- _imagify-helper-plugin/imagify-helper-plugin.php
+++ _imagify-helper-plugin/imagify-helper-plugin.php
@@ -1,37 +1,40 @@
 <?php
 /**
- * Plugin Name: Imagify | {What This Plugin Does}
- * Description: {What this plugin does in one clear sentence.}
- * Plugin URI:  {GitHub repo URL of this plugin}
- * Author:      Imagify Support Team
- * Author URI:  http://imagify.io/
- * License:     GNU General Public License v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Plugin Name:  Imagify | {What This Plugin Does}
+ * Description:  {What this plugin does in one clear sentence.}
+ * Plugin URI:   {GitHub repo URL of this plugin}
+ * Version:      {Plugin version}
+ * Requires PHP: {Required php version: minimum value is 5.3}
+ * Author:       Imagify Support Team
+ * Author URI:   http://imagify.io/
+ * License:      GPLv2
+ * License URI:  http://www.gnu.org/licenses/gpl-2.0.html
  *
- * Copyright SAS WP MEDIA 2018
+ * Copyright 2020 WP Media
+ *
+ * @package WP-Media\ImagifyPluginHelpers\Boilerplate
  */
 
 // EDIT THIS: Replace `boilerplate` with your custom subnamespace.
 // Namespaces must be declared before any other declaration.
-namespace ImagifyPlugin\Helpers\boilerplate;
+namespace ImagifyPluginHelpers\Boilerplate;
 
 // Standard plugin security, keep this line in place.
-defined( 'ABSPATH' ) or die();
+defined( 'ABSPATH' ) || die();
 
+// Hooking into `imagify_loaded` is a safe way to make sure all Imagify features are available, however, it’s not required.
+// Using other hooks directly will be just fine in most cases.
+add_action( 'imagify_loaded', __NAMESPACE__ . '\do_stuff' );
 /**
  * Adds customizations once Imagify has loaded.
  * HEADS UP: If you keep the deactivation hook further down this file,
  * you will have to edit it to remove_filter() this function.
  *
+ * @since  {Plugin version}
  * @author {Author Name}
  */
 function do_stuff() {
-
 	// Do something here.
 	add_filter( 'example_filter', 'example_function' );
 
 }
-// Hooking into `imagify_loaded` is a safe way to make sure all Imagify
-// features are available, however, it’s not required.
-// Using other hooks directly will be just fine in most cases.
-add_action( 'imagify_loaded', __NAMESPACE__ . '\do_stuff' );
