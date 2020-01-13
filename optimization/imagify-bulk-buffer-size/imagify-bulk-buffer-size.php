31de9fa70d0d38b944ff7d057ee7f2cab87b3871
Git I Hate You <git@screenfeed.fr>
2020-01-14 00:54:16 +0100

    Modified the existing helpers to fit the template and the new PHPCS rules.

--- optimization/imagify-bulk-buffer-size/imagify-bulk-buffer-size.php
+++ optimization/imagify-bulk-buffer-size/imagify-bulk-buffer-size.php
@@ -3,30 +3,34 @@
  * Plugin Name: Imagify | Change Bulk Buffer Size
  * Description: Helps to avoid CPU issues during bulk optimization.
  * Plugin URI:  https://github.com/wp-media/imagify-helpers/tree/master/optimization/imagify-bulk-buffer-size/
- * Author:      Imagify Support Team
- * Author URI:  http://imagify.io/
- * License:     GNU General Public License v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Version:      1.1
+ * Requires PHP: 5.3
+ * Author:       Imagify Support Team
+ * Author URI:   http://imagify.io/
+ * License:      GPLv2
+ * License URI:  http://www.gnu.org/licenses/gpl-2.0.html
  *
- * Copyright SAS WP MEDIA 2018
+ * Copyright 2020 WP Media
+ *
+ * @package WP-Media\ImagifyPluginHelpers\BulkBufferSize
  */
-namespace ImagifyPlugin\Helpers\optimization\bulk;
 
-defined( 'ABSPATH' ) or die();
+namespace ImagifyPluginHelpers\Optimization\BulkBufferSize;
+
+defined( 'ABSPATH' ) || die();
 
 add_filter( 'imagify_bulk_buffer_sizes', __NAMESPACE__ . '\buffer_sizes', 10, 3 );
 /**
- * Prevent automatic optimization for PDF.
+ * Allow only one optimization at the same time during bulk optimization, for all contexts.
  *
+ * @since  1.0
  * @author Grégory Viguier
  * @author Caspar Hübinger
  *
- * @param  array $buffer_sizes An array of number of parallel queries
- * @return array               Modified array
+ * @param  array $buffer_sizes An array of number of parallel queries. Array keys are contexts, like 'wp' and 'custom-folders'.
+ * @return array               Modified array.
  */
 function buffer_sizes( $buffer_sizes ) {
-
-	$buffer = array_keys( $buffer_sizes );
-
-	return array_fill_keys( $buffer_sizes, 1 );
+	$contexts = array_keys( $buffer_sizes );
+	return array_fill_keys( $contexts, 1 );
 }
--- optimization/imagify-no-auto-optimize-pdf/imagify-no-auto-optimize-pdf.php
+++ optimization/imagify-no-auto-optimize-pdf/imagify-no-auto-optimize-pdf.php
@@ -3,30 +3,35 @@
  * Plugin Name: Imagify | No Auto-Optimization for PDFs
  * Description: Excludes PDF files from being auto-optimized once they’re uploaded.
  * Plugin URI:  https://github.com/wp-media/imagify-helpers/tree/master/optimization/imagify-no-auto-optimize-pdf/
- * Author:      Imagify Support Team
- * Author URI:  http://imagify.io/
- * License:     GNU General Public License v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Version:      1.0.1
+ * Requires PHP: 5.3
+ * Author:       Imagify Support Team
+ * Author URI:   http://imagify.io/
+ * License:      GPLv2
+ * License URI:  http://www.gnu.org/licenses/gpl-2.0.html
  *
- * Copyright SAS WP MEDIA 2018
+ * Copyright 2020 WP Media
+ *
+ * @package WP-Media\ImagifyPluginHelpers\NoAutoOptimPDF
  */
-namespace ImagifyPlugin\Helpers\optimization\auto;
 
-defined( 'ABSPATH' ) or die();
+namespace ImagifyPluginHelpers\Optimization\NoAutoOptimPDF;
 
-add_filter( 'imagify_auto_optimize_attachment', __NAMESPACE__ . '\no_optimize_pdf', 10, 3 );
+defined( 'ABSPATH' ) || die();
+
+add_filter( 'imagify_auto_optimize_attachment', __NAMESPACE__ . '\no_optimize_pdf', 10, 2 );
 /**
  * Prevent automatic optimization for PDF.
  *
+ * @since  1.0
  * @author Grégory Viguier
  * @author Caspar Hübinger
  *
- * @param  bool  $optimize      True to optimize, false otherwise.
- * @param  int   $attachment_id Attachment ID.
- * @param  array $metadata      An array of attachment meta data.
+ * @param  bool $optimize      True to optimize, false otherwise.
+ * @param  int  $attachment_id Attachment ID.
  * @return bool
  */
-function no_optimize_pdf( $optimize, $attachment_id, $metadata ) {
+function no_optimize_pdf( $optimize, $attachment_id ) {
 	if ( ! $optimize ) {
 		return false;
 	}
