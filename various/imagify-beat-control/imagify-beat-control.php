495ab9d35be7636d4d6841004b3cb47b0760a559
joejoe04 <45705494+joejoe04@users.noreply.github.com>
2021-04-05 15:30:55 -0400

    Add files via upload

---
+++ various/imagify-beat-control/imagify-beat-control.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Imagify | Beat Control
+ * Description: Reduces Imagify Beat interval to once every 120 seconds.
+ * Plugin URI:  https://github.com/wp-media/imagify-helpers/tree/master/various/imagify-beat-control/
+ * Author:      WP Media
+ * Author URI:  https://wp-media.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2020
+ */
+
+namespace ImagifyPlugin\Helpers\various\imagify_beat_control;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) || die();
+
+function ease_heartbeat_interval() {
+
+	add_filter(
+		'imagifybeat_settings',
+		function( $settings ) {
+			// EDIT_HERE
+			$settings['interval'] = 120;
+			// STOP_EDITING
+			return $settings;
+		}
+	);
+}
+add_action( 'imagify_loaded', __NAMESPACE__ . '\ease_heartbeat_interval' );
